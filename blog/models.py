e4383afdc733fcf6a85d49a3f076d355b4d6fcd2
lwooden <lowell.wooden@gmail.com>
2020-06-27 14:59:35 -0400

    create blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    # Metadata Class
+    # Sorts results by the "publish" field in descending order "-"
+    class Meta:
+        ordering = ('-publish',)
+
+        # Getter method that returns the title of the blog
+        def __str__(self):
+            return self.tite
