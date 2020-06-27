1cfc8f1dd80883364d7ce7fa29a82ce543cc5e40
lwooden <lowell.wooden@gmail.com>
2020-06-27 15:08:33 -0400

    comment update

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+# Post Model
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
