06b1371936b656a7b2070c929d081992ae5f321c
lwooden <lowell.wooden@gmail.com>
2020-06-28 09:14:17 -0400

    fixed formatting

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'blog'  # application namespace
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
 ]
