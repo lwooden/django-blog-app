3a12fb1bbf0faf9eca3b6884ba4c7900111d7ae8
lwooden <lowell.wooden@gmail.com>
2020-06-28 18:40:38 -0400

    post list and post detail views

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,23 @@
+from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'post': posts})
+    return render(request,
+                 'blog/post/list.html',
+                 {'posts':posts})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             slug=post,
-                             status='published', 
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-
-    return render(request, '/blog/post/detail.html', {'post': post})
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
