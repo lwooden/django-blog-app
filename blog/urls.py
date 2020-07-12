ce84cc5191ab2d18d7cdb40ba3ee68297bbb24ec
lwooden <lowell.wooden@gmail.com>
2020-07-12 14:35:03 -0400

    add url for share_post

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,7 @@ from . import views
 app_name = 'blog'  # application namespace
 urlpatterns = [
     # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
+    path('', views.PostListView.as_view(), name='post_list'),  # using class based view
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share')
 ]
-
-
-
-
